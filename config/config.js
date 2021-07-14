24844671060acf361858df89ee605038896542de
Manuel Guzman <ale_cba17@hotmail.com>
2021-07-13 23:45:16 -0300

    no env

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 
 const mysqlConfig = {  
     mysql:{
-        host     : process.env.HOST,                  
-        user     : process.env.USER,                        
-        password : process.env.PASSWORD,               
-        database : process.env.DATABASE
+        host     : 'us-cdbr-east-04.cleardb.com',                  
+        user     : 'baa299a13fe6a5',                        
+        password : 'e65d8e91',               
+        database : 'heroku_d7430de00b0cf5e'
     }    
 };
 
